4b2136a74538582879f26043e70e7aa288fb9dc9
khutso-student <khutsomakunyane1@gmail.com>
2025-07-14 19:51:49 +0200

    back-end updated

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,10 @@ const allowedOrigins = [
   'http://localhost:5173',
   'https://week-7-mern-assignment-vius.vercel.app',
   'https://week-7-mern-assignment-ah69.vercel.app',
+  'https://week-7-devops-deployment-assignment-xue3.onrender.com',
+  'https://week-7-mern-assignment.vercel.app'  
 ];
+
 
 // Allow all origins temporarily (debug only)
 // app.use(cors({ origin: '*', credentials: true }));
